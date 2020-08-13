d3fd9bea9f5870b630140f46a93bec66d6e1f0bf
Lanny Rivest <lanny.rivest@gmail.com>
2020-08-13 01:46:38 +0000

    Multiples Bars, Propotions, animation

--- barchartscripts.js
+++ barchartscripts.js
@@ -14,21 +14,34 @@ function updateLabel(){
 
 function  updateValue(){
   let barValueInput = document.getElementsByClassName("quantity");
-  console.log(barValueInput)
   const barValues = Array.from(barValueInput)
   for (let i = 0; i < barValues.length; i++){
     arrOfGraphValues.push(barValues[i].value)
+    arrOfGraphValues[i] = Number(arrOfGraphValues[i])
   }
   console.log(arrOfGraphValues)
 };
 
 function verifyValue(){
+  let invalid = false;
 for (let point of arrOfGraphValues){
- if (isNaN(point) === true || point === "" || point < 0){
- alert("The value of all your bars must be a positive number.");
+  if (isNaN(point) === true || point === 0 || point < 0){
+  alert("The value of all your bars must be a positive number.");
+  invalid = true
+  break;
     }
   }
+  if (invalid === true){
+    location.reload();
+  }
 };
+
+function addMoreBars(){
+  let numExistingBar = document.getElementsByClassName("item").length
+  const newBar = "<input type=\"text\" class=\"item\" placeholder=\"Item...\"></input>" + "\n" + `<input id=\"bar${numExistingBar}Value\" type=\"text\" class=\"quantity\"` +
+  "placeholder=\"Value...\"><br>";
+  $("#addVal").before(newBar)
+}
 
 function createBars(items){
   const graph = document.getElementById("axis");
@@ -37,9 +50,14 @@ function createBars(items){
     bar.setAttribute("id",`bar${i}`);
     bar.setAttribute("class", "bar");
     bar.innerHTML = getBarValue(`bar${i}Value`);
-    bar.style.
+    $(bar).css("width", function(){
+      let ratio = (arrOfGraphValues[i]/Math.max(...arrOfGraphValues)*85) + "%";
+      return ratio
+  })
     graph.appendChild(bar);
   }
+  $(".bar").hide()
+  $(".bar").show("slow")
 };
 
 function getBarValue(barId){
